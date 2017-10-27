09b9803dedba51a2e0440cd107f1f8e6558714c8
joao <joaovitorvlb@hotmail.com>
2017-10-27 21:59:17 -0200

    corrigido configuracao da gpio da galileo

--- sensores.py
+++ sensores.py
@@ -1,21 +1,27 @@
 import time
 from upm import pyupm_temperature as upm
 from upm import pyupm_servo as servo
-from wiringx86 import GPIOGalileo as GPIO
+from wiringx86 import GPIOGalileoGen2 as GPIO
 from upm import pyupm_jhd1313m1 as lcd
 
 pino_sensor_temperatura = 0
-pino_rele = 5
-pino_pot = 15
-pino_servo = 3
+pino_rele  = 3
+pino_led   = 13
+pino_pot   = 15
+pino_servo = 5
 
 pinos = GPIO(debug=False)
+
+
 pinos.pinMode(pino_rele, pinos.OUTPUT)
+pinos.pinMode(pino_led, pinos.OUTPUT)
 pinos.pinMode(pino_pot, pinos.ANALOG_INPUT)
 pinos.pinMode(pino_servo, pinos.OUTPUT)
 temp = upm.Temperature(pino_sensor_temperatura)
 sg_servo = servo.ES08A(pino_servo)
 tela = lcd.Jhd1313m1(0, 0x3E, 0x62)
+
+tela.setColor(0, 100, 50)
 
 def leitura_temperatura():
     return temp.value()
@@ -26,18 +32,23 @@ def leitura_pot():
     return voltagem
 
 def liga_rele():
-    pinos.pinMode(pino_rele, pinos.OUTPUT)
-    pinos.digitalWrite(pino_rele, pinos.HIGH)
+    pinos.digitalWrite(pino_rele,pinos.HIGH)
 
 def desliga_rele():
-    pinos.digitalWrite(pino_rele, pinos.LOW)
+    pinos.digitalWrite(pino_rele,pinos.LOW)
+
+def liga_led():
+    pinos.digitalWrite(pino_led,pinos.HIGH)
+
+def desliga_led():
+    pinos.digitalWrite(pino_led,pinos.LOW)
 
 def move_servo(posicao):
     sg_servo.setAngle(posicao)
 
 def escreve_lcd(texto_linha1, texto_linha2):
     tela.clear()
-    tela.setColor(255, 0, 0)
+    tela.setColor(0, 50, 100)
     tela.setCursor(0, 0)
     isto = str(texto_linha1)
     tela.write(isto)
