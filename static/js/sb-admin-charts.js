385614b3eae0b75f9f4be57eaf255a85877874c1
joao <joaovitorvlb@hotmail.com>
2017-11-21 23:55:35 -0200

    Feitp conficuracoes de atualisacao dinamica dos graficos

--- static/js/sb-admin-charts.js
+++ static/js/sb-admin-charts.js
@@ -1,167 +1,15 @@
-
-$(document).ready(function() 
-{
-    Highcharts.setOptions({
-        global: {
-            useUTC: false
-        }
-      });
-
-      Highcharts.chart('container-grafico-dinamico', {
-          chart: {
-              type: 'spline',
-              animation: Highcharts.svg, // don't animate in old IE
-              marginRight: 10,
-              events: {
-                  load: function () {
-
-                      // set up the updating of the chart each second
-                      var series = this.series[0];
-                      setInterval(function () 
-                      {    
-                          var x = (new Date()).getTime(), // current time
-                              y = Math.random();
-                          series.addPoint([x, y], true, true);
-                      }, 1000);
-                  }
-              }
-          },
-          title: {
-              text: 'Grafico de plotagem dinamica'
-          },
-          xAxis: {
-              type: 'datetime',
-              tickPixelInterval: 150
-          },
-          yAxis: {
-              title: {
-                  text: 'Potencia(W)'
-              },
-              plotLines: [{
-                  value: 0,
-                  width: 1,
-                  color: '#808080'
-              }]
-          },
-          tooltip: {
-              formatter: function () {
-                  return '<b>' + this.series.name + '</b><br/>' +
-                      Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) + '<br/>' +
-                      Highcharts.numberFormat(this.y, 2);
-              }
-          },
-          legend: {
-              enabled: false
-          },
-          exporting: {
-              enabled: false
-          },
-          series: [{
-              name: 'Random data',
-              data: (function () {
-                  // generate an array of random data
-                  var data = [],
-                      time = (new Date()).getTime(),
-                      i;
-
-                  for (i = -19; i <= 0; i += 1) {
-                      data.push({
-                          x: time + i * 1000,
-                          y: Math.random()
-                      });
-                  }
-                  return data;
-              }())
-          }]
-      });
-  });
-
-  var chart = Highcharts.chart('container-grafico-colunas', {
-
-  chart: {
-    type: 'column'
-  },
-
-  title: {
-    text: ''
-  },
-
-  subtitle: {
-    text: ''
-  },
-
-  legend: {
-    align: 'right',
-    verticalAlign: 'middle',
-    layout: 'vertical'
-  },
-
-  xAxis: {
-    categories: ['dom','seg','ter','qua','qui','sex','sab',],
-    labels: {
-      x: -10
-    }
-  },
-
-  yAxis: {
-    allowDecimals: false,
-    title: {
-      text: 'Potencia Durante a Semana'
-    }
-  },
-  
-
-  series: [{
-    name: 'Semana',
-    data: [8, 5, 4, 7, 5, 3, 1]
-  }],
-
-  responsive: {
-    rules: [{
-      condition: {
-        maxWidth: 500
-      },
-      chartOptions: {
-        legend: {
-          align: 'center',
-          verticalAlign: 'bottom',
-          layout: 'horizontal'
-        },
-        yAxis: {
-          labels: {
-            align: 'left',
-            x: 0,
-            y: -5
-          },
-          title: {
-            text: null
-          }
-        },
-        subtitle: {
-          text: null
-        },
-        credits: {
-          enabled: false
-        }
-      }
-    }]
-  }
-});
-
-$('#small').click(function() {
-  chart.setSize(400, 300);
-});
-
-$('#large').click(function() {
-  chart.setSize(600, 300);
-});
+//-------------------------------------------------------------------------------------
+// -------------Criando do grafico de spline dinamico
+//-------------------------------------------------------------------------------------
 
 var chartingOptions = {
     chart: {
-        renderTo: 'container-teste-grafico',
-        type: 'scatter'
+        renderTo: 'container-grafico-dinamico',
+        type: 'spline',
+              animation: Highcharts.svg,
     },
     xAxis: {
+              tickAmount: 10,
               type: 'datetime',
               tickPixelInterval: 150
 
@@ -175,58 +23,116 @@ var chartingOptions = {
               }
           },
     series: [{
-        name: 'Plot',
+        name: 'Tensao do Barramento',
         data: []
 
       },
       {
-        name: 'Plot2',
+        name: 'Corrente consumida',
         data: []
 
       },
       {
-        name: 'Plot3',
+        name: 'Temperatura',
         data: []
 
       }]
 
 };
 var chart = new Highcharts.Chart(chartingOptions);
+//----------------------------------------------------------------------------------------
+// ---------Fim do primeiro grafico
+//----------------------------------------------------------------------------------------
 
+//----------------------------------------------------------------------------------------
+// ---------Criando do grafico de colunas
+//----------------------------------------------------------------------------------------
+var chartingOptions2 = {
+ 
+            chart: {
+                type: 'column',
+                renderTo: 'container-grafico-colunas'
+            },
+            title: {
+                text: 'Monthly Average Rainfall'
+            },
+            subtitle: {
+                text: 'Source: WorldClimate.com'
+            },
+            xAxis: {
+            type: 'category'
+                
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Rainfall (mm)'
+                }
+            },
+            
+            plotOptions: {
+                column: {
+                    pointPadding: 0.2,
+                    borderWidth: 0
+                }
+            },
+            series: [{
+                        name: 'Tokyo',
+                        data: [
+                                ['Shanghai', 23.7],
+                                ['Lagos', 16.1]
+                              ]
+            
+                    }]
+        };
+var chart2 = new Highcharts.Chart(chartingOptions2);
+
+//----------------------------------------------------------------------------------------
+// ---------Fim do grafico de colunas
+//----------------------------------------------------------------------------------------
+
+$('#button2').click(function() 
+{
+
+    chart2.series[0].setData([[ 'oi',5],[ 'tchau',50]]); //Atualiza colunas do grafico
+});
+
+
+   
 function update_test_grafico() 
 {
     $.get("/graficos/1",{},
         function(data)
         {
-            console.log(data);
-            var temp = new Array();
-            var str = new Array();
-            str = String(data);
-            temp = str.split(",");           
-            $('#test-grafico').text(data);
-            $('#test-str1').text(temp[0]);
-            $('#test-str2').text(temp[1]);
-            $('#test-str3').text(temp[2]);
-            $('#test-str4').text(temp[3]);
-            $('#test-str5').text(temp[4]);
-
-
-            est = String(temp[4]);
-            Xt = new Date(est.replace(' ', 'T')).getTime();
-            Y1 = parseInt(temp[1]);
-            Y2 = parseInt(temp[2]);
-            Y3 = parseInt(temp[3]);
-
-
+            Xt = new Date(data[0][4].replace(' ', 'T')).getTime();    //Converte de data para formato calendario Unix
+            Y1 = parseInt(data[0][1]);
+            Y2 = parseInt(data[0][2]);
+            Y3 = parseInt(data[0][3]);
 
             chart.series[0].addPoint({x: Xt, y: Y1}, false);
             chart.series[1].addPoint({x: Xt, y: Y2}, false);
             chart.series[2].addPoint({x: Xt, y: Y3}, false);
             chart.redraw(); 
 
-           var dat = (new Date()).getTime();
-           $('#test-dat').text(dat);
-           $('#test-dat-unx').text(Xt);
+            if(series.data.length > 20)              //Se a quandidade de plotagens for maior que 20
+            {
+              chart.series[0].data[0].remove();      //Remove data 0 da plotagem 0
+              chart.series[1].data[0].remove();      //Remove data 0 da plotagem 0
+              chart.series[2].data[0].remove();      //Remove data 0 da plotagem 0
+            }
         });          
 }
+
+function update_20_grafico() 
+{
+    $.get("/graficos/20",{},
+        function(data)
+        {
+          var tebela = new Array();
+          tebela = data;
+          $('#test-tabela').text(tebela[3][3]);
+        });          
+}
+
+
 
