6dcf23bd79d573dc31cfbb9c7ed804780956b3eb
joao <joaovitorvlb@hotmail.com>
2017-11-25 16:08:13 -0200

    Corrigido acesso a bd a adicionado tabela

--- sqlite_.py
+++ sqlite_.py
@@ -1,14 +1,14 @@
 import sqlite3
+import datetime
 
-def cria_tabela_sensores():
+def cria_tabela_coleta():
     try:
-        conect = sqlite3.connect('site.db')
+        conect = sqlite3.connect('GestaoEnergia.db')
         cursor = conect.cursor()
-        cursor.execute('''CREATE TABLE IF NOT EXISTS sensores
+        cursor.execute('''CREATE TABLE IF NOT EXISTS coleta
                          (id        INTEGER PRIMARY KEY,
-                          corrente REAL, 
-                          tensao   REAL, 
-                          potencia REAL,
+                          potencia REAL, 
+                          bateria   REAL,
                           tempo TIMESTAMP DEFAULT (DATETIME('now')) 
                           )''')
         conect.commit()
@@ -16,22 +16,55 @@ def cria_tabela_sensores():
     except Exception as e:
         print( 'except - cria_tabela_sensores', e)
 
-def retorna_dados_sensores(quantidade=None):
-    conect = sqlite3.connect('site.db')
+def cria_tabela_media():
+    try:
+        conect = sqlite3.connect('GestaoEnergia.db')
+        cursor = conect.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS media
+                         (id        INTEGER PRIMARY KEY,
+                          media REAL, 
+                          bateria   REAL, 
+                          tempo TIMESTAMP DEFAULT (DATETIME('now')) 
+                          )''')
+        conect.commit()
+        conect.close()
+    except Exception as e:
+        print( 'except - cria_tabela_sensores', e)
+
+
+def retorna_dados_coleta(quantidade=None):
+    conect = sqlite3.connect('GestaoEnergia.db')
     cursor = conect.cursor()
     if not quantidade:
-        cursor.execute('''SELECT * FROM sensores ORDER BY datetime(tempo) ASC''')
+        cursor.execute('''SELECT * FROM coleta ORDER BY datetime(tempo) ASC''')
     else:
-        cursor.execute('''SELECT * FROM sensores ORDER BY datetime(tempo) DESC LIMIT ?''', (quantidade,))
+        cursor.execute('''SELECT * FROM coleta ORDER BY datetime(tempo) DESC LIMIT ?''', (quantidade,))
     return cursor.fetchall()
 
-def adiciona_dado_sensores(corrente, tensao, potencia):
+def adiciona_dado_coleta(potencia, bateria):
     try:
-        conect = sqlite3.connect('site.db')
+        conect = sqlite3.connect('GestaoEnergia.db')
         cursor = conect.cursor()
         tempo  = datetime.datetime.now()
-        cursor.execute('''INSERT INTO sensores (tempo, corrente, tensao, potencia)
-                          VALUES(?,?,?,?)''',(tempo, corrente, tensao, potencia))
+        cursor.execute('''INSERT INTO coleta (potencia, bateria, tempo)
+                          VALUES(?,?,?)''',(potencia, bateria, tempo))
+        conect.commit()
+        conect.close()
+        if cursor.rowcount > 0:
+            return True
+        else:
+            return False
+    except Exception as e:
+        print( e)
+        return False
+
+def adiciona_dado_media(media, tempo):
+    try:
+        conect = sqlite3.connect('GestaoEnergia.db')
+        cursor = conect.cursor()
+        tempo  = datetime.datetime.now()
+        cursor.execute('''INSERT INTO media (media, tempo)
+                          VALUES(?,?)''',(media, tempo))
         conect.commit()
         conect.close()
         if cursor.rowcount > 0:
