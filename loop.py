4f966d098924cafdff441109294dbf81b4b0c479
joao <joaovitorvlb@hotmail.com>
2017-11-25 17:15:03 -0200

    Feito algoritimo de setpoint de data

--- loop.py
+++ loop.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from threading import Thread
 import sqlite_
 import datetime
@@ -8,13 +10,21 @@ class Loop(Thread):
 		Thread.__init__(self)
 
 	def run(self):
+		flag = 1
 		while True:
 			now = datetime.datetime.now()
 			print now
-			setp = now.replace(hour=8, minute=17, second=0, microsecond=0)
+			setp = now.replace(second=50, microsecond=0)
+			print " "
 			print setp
 			print " "
-			if setp < now:
-				sqlite_.cria_tabela_media()
-				sqlite_.adiciona_dado_media(now.minute, now.second)
+			if setp > now:                           #Indica quando chega um novo dia
+				flag = 1	                         #flag para selar atá a proxima condição
+
+			if setp < now:                           #Checa se a data atual ja é a mesma doset point 
+				if flag == 1:                        #flag indica se o dia atual na teve media apurada
+					flag = 0                         #flag para selar atá a proxima condição
+					print "Fim do minuto!"
+					sqlite_.cria_tabela_media()
+					sqlite_.adiciona_dado_media(now.minute, now.second)
 			time.sleep(1)
