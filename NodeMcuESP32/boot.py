6383743b13fb64831378fb4e046d9774c3ef4cfd
joao <joaovitorvlb@hotmail.com>
2018-02-03 23:03:48 -0200

    configurado boot do esp32

--- NodeMcuESP32/boot.py
+++ NodeMcuESP32/boot.py
@@ -1,7 +1,13 @@
-# This file is executed on every boot (including wake-boot from deepsleep)
-import main
 
-machine.main('main.py')
-main.start()
+# This file is executed on every boot (including wake-boot from deepsleep)
+
+#import ina219 
+import machine
+import os
+#import main
+
+
+#machine.main('main.py')
+#main.start()
 
 
