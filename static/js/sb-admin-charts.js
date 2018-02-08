d61e992f87940d50d1d5f261e39804fd86bd3976
joao <joaovitorvlb@hotmail.com>
2018-02-08 00:30:53 -0200

    Adicionado 3 gauges para o acelerometro

--- static/js/sb-admin-charts.js
+++ static/js/sb-admin-charts.js
@@ -44,6 +44,7 @@ var chart = new Highcharts.Chart(chartingOptions);
 // ---------Fim do primeiro grafico
 //----------------------------------------------------------------------------------------
 
+
 //----------------------------------------------------------------------------------------
 // ---------Criando do grafico de colunas
 //----------------------------------------------------------------------------------------
@@ -92,10 +93,470 @@ var chartingOptions2 = {
         };
 var chart2 = new Highcharts.Chart(chartingOptions2);
 
+
+
 //----------------------------------------------------------------------------------------
 // ---------Fim do grafico de colunas
 //----------------------------------------------------------------------------------------
 
+//----------------------------------------------------------------------------------------
+// ---------Criando o gauge 1
+//----------------------------------------------------------------------------------------
+var gauge1 = {
+
+    chart: {
+        type: 'gauge',
+        renderTo:"test-gauge1",
+        plotBackgroundColor: null,
+        plotBackgroundImage: null,
+        plotBorderWidth: 0,
+        plotShadow: false
+    },
+
+    title: {
+        text: 'Speedometer'
+    },
+
+    pane: {
+        startAngle: -150,
+        endAngle: 150,
+        background: [{
+            backgroundColor: {
+                linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                stops: [
+                    [0, '#FFF'],
+                    [1, '#333']
+                ]
+            },
+            borderWidth: 0,
+            outerRadius: '109%'
+        }, {
+            backgroundColor: {
+                linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                stops: [
+                    [0, '#333'],
+                    [1, '#FFF']
+                ]
+            },
+            borderWidth: 1,
+            outerRadius: '107%'
+        }, {
+            // default background
+        }, {
+            backgroundColor: '#DDD',
+            borderWidth: 0,
+            outerRadius: '105%',
+            innerRadius: '103%'
+        }]
+    },
+
+    // the value axis
+    yAxis: {
+        min: 0,
+        max: 200,
+
+        minorTickInterval: 'auto',
+        minorTickWidth: 1,
+        minorTickLength: 10,
+        minorTickPosition: 'inside',
+        minorTickColor: '#666',
+
+        tickPixelInterval: 30,
+        tickWidth: 2,
+        tickPosition: 'inside',
+        tickLength: 10,
+        tickColor: '#666',
+        labels: {
+            step: 2,
+            rotation: 'auto'
+        },
+        title: {
+            text: 'km/h'
+        },
+        plotBands: [{
+            from: 0,
+            to: 120,
+            color: '#55BF3B' // green
+        }, {
+            from: 120,
+            to: 160,
+            color: '#DDDF0D' // yellow
+        }, {
+            from: 160,
+            to: 200,
+            color: '#DF5353' // red
+        }]
+    },
+
+    series: [{
+        name: 'Speed',
+        data: [80],
+        tooltip: {
+            valueSuffix: ' km/h'
+        }
+    }]
+
+};
+
+var gauges1 = new Highcharts.Chart(gauge1);
+
+
+
+//----------------------------------------------------------------------------------------
+// ---------Fim do gauge 1
+//----------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------
+// ---------Criando o gauge 1
+//----------------------------------------------------------------------------------------
+var gauge2 = {
+
+    chart: {
+        type: 'gauge',
+        renderTo:"test-gauge2",
+        plotBackgroundColor: null,
+        plotBackgroundImage: null,
+        plotBorderWidth: 0,
+        plotShadow: false
+    },
+
+    title: {
+        text: 'Speedometer'
+    },
+
+    pane: {
+        startAngle: -150,
+        endAngle: 150,
+        background: [{
+            backgroundColor: {
+                linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                stops: [
+                    [0, '#FFF'],
+                    [1, '#333']
+                ]
+            },
+            borderWidth: 0,
+            outerRadius: '109%'
+        }, {
+            backgroundColor: {
+                linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                stops: [
+                    [0, '#333'],
+                    [1, '#FFF']
+                ]
+            },
+            borderWidth: 1,
+            outerRadius: '107%'
+        }, {
+            // default background
+        }, {
+            backgroundColor: '#DDD',
+            borderWidth: 0,
+            outerRadius: '105%',
+            innerRadius: '103%'
+        }]
+    },
+
+    // the value axis
+    yAxis: {
+        min: 0,
+        max: 200,
+
+        minorTickInterval: 'auto',
+        minorTickWidth: 1,
+        minorTickLength: 10,
+        minorTickPosition: 'inside',
+        minorTickColor: '#666',
+
+        tickPixelInterval: 30,
+        tickWidth: 2,
+        tickPosition: 'inside',
+        tickLength: 10,
+        tickColor: '#666',
+        labels: {
+            step: 2,
+            rotation: 'auto'
+        },
+        title: {
+            text: 'km/h'
+        },
+        plotBands: [{
+            from: 0,
+            to: 120,
+            color: '#55BF3B' // green
+        }, {
+            from: 120,
+            to: 160,
+            color: '#DDDF0D' // yellow
+        }, {
+            from: 160,
+            to: 200,
+            color: '#DF5353' // red
+        }]
+    },
+
+    series: [{
+        name: 'Speed',
+        data: [80],
+        tooltip: {
+            valueSuffix: ' km/h'
+        }
+    }]
+
+};
+var gauges2 = new Highcharts.Chart(gauge2);
+
+
+//----------------------------------------------------------------------------------------
+// ---------Criando o gauge 1
+//----------------------------------------------------------------------------------------
+var gauge3 = {
+
+    chart: {
+        type: 'gauge',
+        renderTo:"test-gauge3",
+        plotBackgroundColor: null,
+        plotBackgroundImage: null,
+        plotBorderWidth: 0,
+        plotShadow: false
+    },
+
+    title: {
+        text: 'Speedometer'
+    },
+
+    pane: {
+        startAngle: -150,
+        endAngle: 150,
+        background: [{
+            backgroundColor: {
+                linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                stops: [
+                    [0, '#FFF'],
+                    [1, '#333']
+                ]
+            },
+            borderWidth: 0,
+            outerRadius: '109%'
+        }, {
+            backgroundColor: {
+                linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                stops: [
+                    [0, '#333'],
+                    [1, '#FFF']
+                ]
+            },
+            borderWidth: 1,
+            outerRadius: '107%'
+        }, {
+            // default background
+        }, {
+            backgroundColor: '#DDD',
+            borderWidth: 0,
+            outerRadius: '105%',
+            innerRadius: '103%'
+        }]
+    },
+
+    // the value axis
+    yAxis: {
+        min: 0,
+        max: 200,
+
+        minorTickInterval: 'auto',
+        minorTickWidth: 1,
+        minorTickLength: 10,
+        minorTickPosition: 'inside',
+        minorTickColor: '#666',
+
+        tickPixelInterval: 30,
+        tickWidth: 2,
+        tickPosition: 'inside',
+        tickLength: 10,
+        tickColor: '#666',
+        labels: {
+            step: 2,
+            rotation: 'auto'
+        },
+        title: {
+            text: 'km/h'
+        },
+        plotBands: [{
+            from: 0,
+            to: 120,
+            color: '#55BF3B' // green
+        }, {
+            from: 120,
+            to: 160,
+            color: '#DDDF0D' // yellow
+        }, {
+            from: 160,
+            to: 200,
+            color: '#DF5353' // red
+        }]
+    },
+
+    series: [{
+        name: 'Speed',
+        data: [80],
+        tooltip: {
+            valueSuffix: ' km/h'
+        }
+    }]
+
+};
+var gauges3 = new Highcharts.Chart(gauge3);
+
+
+
+
+
+var gaugeOptions = {
+
+    chart: {
+        type: 'solidgauge'
+    },
+
+    title: null,
+
+    pane: {
+        center: ['50%', '85%'],
+        size: '140%',
+        startAngle: -90,
+        endAngle: 90,
+        background: {
+            backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || '#EEE',
+            innerRadius: '60%',
+            outerRadius: '100%',
+            shape: 'arc'
+        }
+    },
+
+    tooltip: {
+        enabled: false
+    },
+
+    // the value axis
+    yAxis: {
+        stops: [
+            [0.1, '#55BF3B'], // green
+            [0.5, '#DDDF0D'], // yellow
+            [0.9, '#DF5353'] // red
+        ],
+        lineWidth: 0,
+        minorTickInterval: null,
+        tickAmount: 2,
+        title: {
+            y: -70
+        },
+        labels: {
+            y: 16
+        }
+    },
+
+    plotOptions: {
+        solidgauge: {
+            dataLabels: {
+                y: 5,
+                borderWidth: 0,
+                useHTML: true
+            }
+        }
+    }
+};
+
+// The speed gauge
+var chartSpeed = Highcharts.chart('container-speed', Highcharts.merge(gaugeOptions, {
+    yAxis: {
+        min: 0,
+        max: 200,
+        title: {
+            text: 'Speed'
+        }
+    },
+
+    credits: {
+        enabled: false
+    },
+
+    series: [{
+        name: 'Speed',
+        data: [80],
+        dataLabels: {
+            format: '<div style="text-align:center"><span style="font-size:25px;color:' +
+                ((Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black') + '">{y}</span><br/>' +
+                   '<span style="font-size:12px;color:silver">km/h</span></div>'
+        },
+        tooltip: {
+            valueSuffix: ' km/h'
+        }
+    }]
+
+}));
+
+// The RPM gauge
+var chartRpm = Highcharts.chart('container-rpm', Highcharts.merge(gaugeOptions, {
+    yAxis: {
+        min: 0,
+        max: 5,
+        title: {
+            text: 'RPM'
+        }
+    },
+
+    series: [{
+        name: 'RPM',
+        data: [1],
+        dataLabels: {
+            format: '<div style="text-align:center"><span style="font-size:25px;color:' +
+                ((Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black') + '">{y:.1f}</span><br/>' +
+                   '<span style="font-size:12px;color:silver">* 1000 / min</span></div>'
+        },
+        tooltip: {
+            valueSuffix: ' revolutions/min'
+        }
+    }]
+
+}));
+
+// Bring life to the dials
+setInterval(function () {
+    // Speed
+    var point,
+        newVal,
+        inc;
+
+    if (chartSpeed) {
+        point = chartSpeed.series[0].points[0];
+        inc = Math.round((Math.random() - 0.5) * 100);
+        newVal = point.y + inc;
+
+        if (newVal < 0 || newVal > 200) {
+            newVal = point.y - inc;
+        }
+
+        point.update(newVal);
+    }
+
+    // RPM
+    if (chartRpm) {
+        point = chartRpm.series[0].points[0];
+        inc = Math.random() - 0.5;
+        newVal = point.y + inc;
+
+        if (newVal < 0 || newVal > 5) {
+            newVal = point.y - inc;
+        }
+
+        point.update(newVal);
+    }
+}, 2000);
+
+
+
+//----------------------------------------------------------------------------------------
+// ---------Fim do gauge 1
+//----------------------------------------------------------------------------------------
 $('#button2').click(function() 
 {
 
@@ -121,36 +582,35 @@ $(document).ready(
                 var x4 = new Date(data[3][3].replace(' ', 'T')).getTime();
                 var x5 = new Date(data[4][3].replace(' ', 'T')).getTime();
                 var x6 = new Date(data[5][3].replace(' ', 'T')).getTime();
-                var x7 = new Date(data[7][3].replace(' ', 'T')).getTime();
+                var x7 = new Date(data[6][3].replace(' ', 'T')).getTime();
                 var x8 = new Date(data[7][3].replace(' ', 'T')).getTime();
 
-                var y1 = parseInt(data[0][1]);
-                var y2 = parseInt(data[1][1]);
-                var y3 = parseInt(data[2][1]);
-                var y4 = parseInt(data[3][1]);
-                var y5 = parseInt(data[4][1]);
-                var y6 = parseInt(data[5][1]);
-                var y7 = parseInt(data[6][1]);
-                var y8 = parseInt(data[7][1]);
+                var y1 = parseInt(data[0][3]);
+                var y2 = parseInt(data[1][3]);
+                var y3 = parseInt(data[2][3]);
+                var y4 = parseInt(data[3][3]);
+                var y5 = parseInt(data[4][3]);
+                var y6 = parseInt(data[5][3]);
+                var y7 = parseInt(data[6][3]);
+                var y8 = parseInt(data[7][3]);
 
-                  
-                $('#test-col').text("tchau");
               
-              chart2.series[0].setData([
-                                          [ x1 , y1 ],
-                                          [ x2 , y2 ],
-                                          [ x3 , y3 ],
-                                          [ x4 , y4 ],
-                                          [ x5 , y5 ],
-                                          [ x6 , y6 ],
-                                          [ x7 , y7 ],
-                                          [ x8 , y8 ]
-                                        ]); //Atualiza colunas do grafic0
+                chart2.series[0].setData([
+                                            [ x1 , y1 ],
+                                            [ x2 , y2 ],
+                                            [ x3 , y3 ],
+                                            [ x4 , y4 ],
+                                            [ x5 , y5 ],
+                                            [ x6 , y6 ],
+                                            [ x7 , y7 ],
+                                            [ x8 , y8 ]
+                                          ]); //Atualiza colunas do grafic0
               });
         })
 
 
-   
+var teste = 6; 
+
 function update_test_grafico() 
 {
     $.get("/graficos/1",{},   //Solicita get ao servidor e aguarda a ultima lina do BD
@@ -158,7 +618,7 @@ function update_test_grafico()
         {
             var series = chart.series[0];
 
-            Xt = new Date(data[0][3].replace(' ', 'T')).getTime();    //Converte de data para formato calendario Unix
+            Xt = new Date(data[0][5].replace(' ', 'T')).getTime();    //Converte de data para formato calendario Unix
             Y1 = parseInt(data[0][1]);        //Garante que os valores sejam inteiros
             Y2 = parseInt(data[0][2]);        //Garante que os valores sejam inteiros
             Y3 = parseInt(data[0][3]);        //Garante que os valores sejam inteiros
@@ -176,6 +636,7 @@ function update_test_grafico()
             }
 
             $('#test-str1').text(series.data.length);
+            $('#test-str2').text(teste);
         });          
 }
 
