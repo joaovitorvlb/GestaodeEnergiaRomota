67433dfb53f9a46b33087c6e3b0bd480c357f2cf
joao <joaovitorvlb@hotmail.com>
2018-02-10 19:47:20 -0200

    iclusao da bibioteca grove lcd no boot do micropython

--- NodeMcuESP32/boot.py
+++ NodeMcuESP32/boot.py
@@ -1,13 +1,17 @@
 
 # This file is executed on every boot (including wake-boot from deepsleep)
 
-#import ina219 
+import ina219 
 import machine
 import os
-#import main
+import main
+import lcd
+import i2c_lcd
+import i2c_lcd_backlight
+import i2c_lcd_screen
 
 
-#machine.main('main.py')
-#main.start()
+machine.main('main.py')
+main.start()
 
 
