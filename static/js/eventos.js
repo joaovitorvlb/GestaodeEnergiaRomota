0bf1177f06bde3f507715a7d3002a42c525d0220
joao <joaovitorvlb@hotmail.com>
2018-02-17 20:45:12 -0200

    Configurado eventos

--- static/js/eventos.js
+++ static/js/eventos.js
@@ -22,37 +22,43 @@ function update()
     function(data)
     {
         console.log(data);
-        point =gauges1.series[0].points[0];
-        point.update(data[0].toFixed(2) / 1000);
 
-        point =gauges2.series[0].points[0];
-        point.update(data[1].toFixed(2) / 1000);
-
-        point =gauges3.series[0].points[0];
-        point.update(data[2].toFixed(2) / 1000);
-
+        $('#p_disc').text(data[5].toFixed(2) + ' %');
         $('#cpu_perc').text(data[3].toFixed(2) + ' %');
         $('#p_mem').text(data[4].toFixed(2) + ' %');
-        $('#p_disc').text(data[5].toFixed(2) + ' %');
+        
 
         slider.setValue(data[4].toFixed(2));
 
+        vr = parseInt(data[6]) / 1000;
+        point =gauges1.series[0].points[0];
+        point.update(vr);
+
+        vr = parseInt(data[7]) / 1000;
+        point =gauges2.series[0].points[0];
+        point.update(vr);
+
+        vr = parseInt(data[8]) / 1000;
+        point =gauges3.series[0].points[0];
+        point.update(vr);     
+        
+        vr = parseInt(data[0]) / 1000;
         point =gauges4.series[0].points[0];
-        point.update(data[6].toFixed(2) / 1000);
+        point.update(vr);
 
+        vr = parseInt(data[1]) / 1000;
         point =gauges5.series[0].points[0];
-        point.update(data[7].toFixed(2) / 1000);
+        point.update(vr);
 
+        vr = parseInt(data[2]) / 1000;
         point =gauges6.series[0].points[0];
-        point.update(data[8].toFixed(2) / 1000);
+        point.update(vr);
 
         vr = parseInt(data[9])
-
         point =gauges7.series[0].points[0];
         point.update(vr);
 
         vr = parseInt(data[10])
-
         point =gauges8.series[0].points[0];
         point.update(vr);
     });
@@ -74,18 +80,6 @@ function update_grafico()
 //----------------------------------------------------------------------------------------
 // ---------Inicio dos eventos dos graficos
 //----------------------------------------------------------------------------------------
-$('#button2').click(function() 
-{
-
-            chart2.series[0].setData([
-                                        [ 1511411609545 ,5 ],
-                                        [ 1511411625470 ,50],
-                                        [ 1511411635870 ,3 ],
-                                        [ 1511411646270 ,45],
-                                        [ 1511411656695 ,56],
-                                        [ 1511411661895 ,22]
-                                      ]); //Atualiza colunas do grafic0
-        });
 
 $(document).ready(
         function()
@@ -102,14 +96,14 @@ $(document).ready(
                 var x7 = new Date(data[6][3].replace(' ', 'T')).getTime();
                 var x8 = new Date(data[7][3].replace(' ', 'T')).getTime();
 
-                var y1 = parseInt(data[0][3]);
-                var y2 = parseInt(data[1][3]);
-                var y3 = parseInt(data[2][3]);
-                var y4 = parseInt(data[3][3]);
-                var y5 = parseInt(data[4][3]);
-                var y6 = parseInt(data[5][3]);
-                var y7 = parseInt(data[6][3]);
-                var y8 = parseInt(data[7][3]);
+                var y1 = parseInt(data[0][1]);
+                var y2 = parseInt(data[1][1]);
+                var y3 = parseInt(data[2][1]);
+                var y4 = parseInt(data[3][1]);
+                var y5 = parseInt(data[4][1]);
+                var y6 = parseInt(data[5][1]);
+                var y7 = parseInt(data[6][1]);
+                var y8 = parseInt(data[7][1]);
 
               
                 chart2.series[0].setData([
@@ -136,16 +130,12 @@ function update_test_grafico()
             var series = chart.series[0];
 
             Xt = new Date(data[0][5].replace(' ', 'T')).getTime();    //Converte de data para formato calendario Unix
-            Y1 = parseInt(data[0][1]);        //Garante que os valores sejam inteiros
-            Y2 = parseInt(data[0][2]);        //Garante que os valores sejam inteiros
-            Y3 = parseInt(data[0][3]);        //Garante que os valores sejam inteiros
-
+            Y1 = parseFloat(data[0][1]);        //Garante que os valores sejam inteiros
+         
 
             if(v_data != Xt)
             {
                 chart.series[0].addPoint({x: Xt, y: Y1}, false);
-                chart.series[1].addPoint({x: Xt, y: Y2}, false);
-                chart.series[2].addPoint({x: Xt, y: Y3}, false);
                 chart.redraw();
                 v_data = Xt;
             }
